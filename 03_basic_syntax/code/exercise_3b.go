ff664d4e8d0cd2b8dfb19bfac29bdd4fee23fba3
DallasCarraher <dallas.carraher@gmail.com>
2021-01-18 13:17:56 -0800

    complete exercise 3b

---
+++ 03_basic_syntax/code/exercise_3b.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var name, hometown string
+	var years int
+	var weather string
+
+	fmt.Print("Enter your name: ")
+	fmt.Scan(&name)
+	fmt.Print("Enter your hometown: ")
+	fmt.Scan(&hometown)
+	fmt.Print("Enter how many years you've lived there: ")
+	fmt.Scan(&years)
+	fmt.Print("Is the weather there nice?: ")
+	fmt.Scan(&weather)
+
+	weather = strings.ToUpper(weather)
+	if weather == "YES" {
+		weather = "true"
+	} else {
+		weather = "false"
+	}
+	weatherBool, _ := strconv.ParseBool(weather)
+
+	fmt.Printf("Hi! My name is %s. I have lived in %s for %d years. They say the weather is amazing, which is %t.", name, hometown, int(years), weatherBool)
+}
